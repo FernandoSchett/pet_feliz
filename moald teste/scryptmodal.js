b5f12609a5131ec0ba6b45f02a94e20031c2c27f
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2022-04-15 14:50:01 -0300

    adicionando novos modais.

--- moald teste/scryptmodal.js
+++ moald teste/scryptmodal.js
@@ -1,6 +1,6 @@
 var modalBtns = document.querySelectorAll(".modal-open");
 
-modalBtns.forEach(function(btn){ //Abrir os modais
+modalBtns.forEach(function(btn){
     btn.onclick = function(){
         var modal = btn.getAttribute("data-modal");
         document.getElementById(modal).style.display = "block";
@@ -9,16 +9,15 @@ modalBtns.forEach(function(btn){ //Abrir os modais
 
 var closeBtns = document.querySelectorAll(".modal-close");
 
-closeBtns.forEach(function(btn){ //Fechar os Modais
+closeBtns.forEach(function(btn){
     btn.onclick = function(){
         var modal = (btn.closest(".modal").style.display = "none");
     };
 });
 
-window.onclick = function(e){ //Fechar modais ao clicar na tela
-    if(e.target.className === "modal" ){
+window.onclick = function(e){
+    if(e.target.className === "modal"){
         e.target.style.display = "none";
     }
-}
 
-// || e.target.className == "modal-content" || e.target.className == "modal-header" || e.target.className == "modal-body" || e.target.className == "tectocima" || e.target.className == "animalmodal"   
+}
