29743b5e8f080ce09327da780bb818722e9ffc90
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2022-04-15 16:10:29 -0300

    Prototipagem e documentacao
    Adicao da prototipagem

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var modalBtns = document.querySelectorAll(".modal-open"); //seleciona aquele que tem classe modal-open
+var modalBtns = document.querySelectorAll(".modal-open");
 
-modalBtns.forEach(function(btn){ //Abrir os modais, de acordo com o ID
+modalBtns.forEach(function(btn){ //Abrir os modais
     btn.onclick = function(){
         var modal = btn.getAttribute("data-modal");
         document.getElementById(modal).style.display = "block";
     };
 });
 
-var closeBtns = document.querySelectorAll(".modal-close"); //seleciona aquele que tem classe modal-close
+var closeBtns = document.querySelectorAll(".modal-close");
 
-closeBtns.forEach(function(btn){ //Fechar o Modal aberto, no botao
+closeBtns.forEach(function(btn){ //Fechar os Modais
     btn.onclick = function(){
         var modal = (btn.closest(".modal").style.display = "none");
     };
@@ -21,3 +21,4 @@ window.onclick = function(e){ //Fechar modais ao clicar na tela
     }
 }
 
+// || e.target.className == "modal-content" || e.target.className == "modal-header" || e.target.className == "modal-body" || e.target.className == "tectocima" || e.target.className == "animalmodal"   
